c21b9ffbec92677a5e3449eddc756b42431b513f
Vadim Krasnov <2vadimkrasnov@gmail.com>
2023-10-16 20:26:47 +0300

    просто пытаемся запушить из гита

--- src/smthToBE.java
+++ src/smthToBE.java
@@ -1,5 +1,6 @@
 public class smthToBE {
     public static void main(String[] args) {
         System.out.println("просто пытаемся запушить из гита");
+        System.out.println("не вышло");
     }
 }
